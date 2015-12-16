0cf405d3256feae71b45f9da2e0f342f67885795
Cory Bennett <cbennett4@liberty.edu>
2015-12-15 21:09:55 -0500

    final

---
+++ final/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+            $('#candidateDiv').hide();
+            $('#panelistDiv').hide();
+            $("#panelist").click(function() {
+                $('#panelistDiv').show();
+                $('#candidateDiv').hide();
+            })
+            $("#candidate").click(function() {
+                $('#panelistDiv').hide();
+                $('#candidateDiv').show();
+            })
+            $('#panelistSection').click(function() {
+                $('#panelistDiv').append(
+                    "<div class='draggable' style=height:50px;width:100px;border:1px solid black;>Panelist</div>"
+                )
+                $('.draggable').draggable();
+            })
+            $('#candidateSection').click(function() {
+                $('#candidateDiv').append(
+                    "<div class='draggable' style='height:50px;width:100px;border:1px solid black;'>Candidate</div>"
+                );
+                $('.draggable').draggable();
+            })
+            jsforce.browser.init({
+                clientId: '3MVG9KI2HHAq33RwjKIHlvM0rlEcHeN3BZ3hIRJURxgi6qvVu90OSDbjIUQK5KBnmPWMnegMLs0RjK8Z67ozr',
+                redirectUri: 'https://salesforce.com/services/oauth2/token'
+            });
+            jsforce.browser.on('connect', function(conn) {
+                conn.query('SELECT Id, Name FROM CampaignMember ', function(err,
+                    res) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log(res);
+                });
+            });
+})
