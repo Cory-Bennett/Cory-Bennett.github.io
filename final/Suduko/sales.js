fb8ee388915f16677001d2e405e6c2a6d7b0fff8
Cory Bennett <cbennett4@liberty.edu>
2016-01-05 18:43:19 -0500

    sudo

--- final/Suduko/sales.js
+++ final/Suduko/sales.js
@@ -22,10 +22,29 @@ app.get('/', function(req, res){
     console.log("Org ID: " + userInfo.organizationId);
 
     conn.query('SELECT Name FROM Account', function(err, result){
-      res.send(result);
+      var names = result.records.map(function(item){
+        return item.Name;
+      });
+      // names.each(function(item) {
+      //   $(body).append('<div>' + item + '</div>')
+      // });
+      res.send(names);
     });
 
   });
 }) 
+
+// var pg = require('pg');
+
+// pg.connect(process.env.postgres://eihehaoxkmmmpt:H9alk1QC4G7CcHyrJPNviE2Z9M@ec2-54-204-8-224.compute-1.amazonaws.com:5432/dcc9tudratrchu, function(err, client) {
+//   if (err) throw err;
+//   console.log('Connected to postgres! Getting schemas...');
+
+//   client
+//     .query('SELECT Name FROM Account');
+//     .on('row', function(row) {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
 app.listen('8888', function(){console.log('server running')})
